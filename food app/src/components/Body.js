75178e8c332a95918b8bbe1cdc7aa9215df67d76
Yatharth Gupta <guptayatharth.yg@gmail.com>
2024-01-27 06:57:20 +0530

    added shimmer search funtionality

--- food app/src/components/Body.js
+++ food app/src/components/Body.js
@@ -1,26 +1,56 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import resList from "../utils/MockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
-  useEffect(()=>{
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [searchText, setSearchText] = useState([]);
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
-  const fetchData=async()=>{
-    const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.635009&lng=77.282939&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    const json=await data.json();
-    console.log(json);
-    setListOfRestraunt(json.data.cards[3].card.card.gridElements.infoWithStyle.restaurants)
-  
-
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.635009&lng=77.282939&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    //   console.log(json);
+    setListOfRestraunt(
+      json.data.cards[3].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+  // console.log(resList)
+  if (listOfRestaurants.length === 0 && searchText.length===0) {
+    return <Shimmer />;
   }
-  console.log(resList)
+  else 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="searchBtn"
+            onClick={() => {
+              const searchList = listOfRestaurants.filter(
+                (res) => res.info.name.toLowerCase().includes(searchText)
+              );
+              setListOfRestraunt(searchList);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        {console.log(searchText)}
         <button
           className="filter-btn"
           onClick={() => {
@@ -34,6 +64,14 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
+        {/* {three ways to condiotional render 
+        one is below
+        one is using ternary operator as it restaurants
+        one is using && to render } */}
+      {(()=>{if(listOfRestaurants.length === 0 && searchText.length !== 0)  return(
+            <div>No restaurants found</div>
+          );})()}
+
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
--- food app/src/components/Header.js
+++ food app/src/components/Header.js
@@ -1,6 +1,7 @@
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
 const Header = () => {
+  const [btnName, setbtnName] = useState("login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +13,17 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button
+              className="login"
+              onClick={() => {
+                if (btnName === "login") setbtnName("logout");
+                else if (btnName === "logout") setbtnName("login");
+              }}
+            >
+              {btnName}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
